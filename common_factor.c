c5c1e6772968a9a9596aa2230d35e7fa15c53526
AnwarKawsar <kawsar0anwar@gmail.com>
2023-10-03 21:12:03 +0600

    h

--- common_factor.c
+++ common_factor.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 int main(){
-    int x,y,z, count = 0;
-    printf("Num1:");
-    scanf("%d",&x);
-    printf("Num2:");
-    scanf("%d",&y);
-    if (x > y) {
-        for (int i = 1; i <= y; i++) {
-            if (x % i == 0 && y % i == 0) {
-            count++;
+    int a,b,i=0,count=0;
+    scanf("%d%d",&a,&b);
+    if(a<b){
+        while(i<=a){
+            i++;
+            if(a%i==0&&b%i==0){
+                count++;
+                
             }
-        }   printf("\n %d",count);
-    }else {
-        for (int i = 1; i <= x; i++) {
-            if (x % i == 0 && y % i == 0) {
-            count++;}
-        }printf("\n %d",count);
-    }
-return 0;
- }
+        }
+    }else{
+        while(i<=b){
+            i++;
+            if(a%i==0&&b%i==0){
+                count++;
+               
+            }
+        }
+    }printf("%d",count);
+    return 0;
+}
 
 
